066aa94316154d91b73a6fd7903df560c63cde4d
Karol Ciszek <kjc15@ic.ac.uk>
2021-09-10 23:25:04 +0100

    Make GET request to Unsplash API
    This GET request is performed by default on page load. API key not
    provided

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-(function () {console.log("Hello world!");})();
+// Global function closure
+(function () {
+  const API_ROOT = 'https://api.unsplash.com';
+  const ACCESS_KEY = 'in_progress';
+  const photosURI = `${API_ROOT}/photos/?client_id=${ACCESS_KEY}`;
+
+  function fetchPhotos () {
+    return fetch(photosURI)
+      .then(response => response.json());
+  }
+
+  console.log("Hello world!");
+
+  fetchPhotos()
+    .then(photos => console.log(photos))
+    .catch(err => console.log(err));
+})();
