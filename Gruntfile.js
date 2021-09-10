5c6a7b272c81757534a16062f05dd919fd80b2fd
Karol Ciszek <kjc15@ic.ac.uk>
2021-09-10 22:28:02 +0100

    Create basic Grunt workflow
    This workflow performs linting on JS code, then executes SASS preprocessor
    and copies resulting files into dist/

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,15 +9,41 @@ module.exports = function(grunt) {
         }
       }
     },
+
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
-    }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'dist/css/main.css': 'src/css/main.scss'
+        }
+      }
+    },
+
+    copy: {
+      // Copy plugin is used to move any remaining files to the dist/ subdirectory
+      main: {
+        files: [
+          // Move all files (not dirs) from src/ into our dist/ directory
+          {expand: true, cwd: 'src/', src: ['*'], dest: 'dist/', filter: 'isFile'},
+          {expand: true, cwd: 'src/js', src: ['*'], dest: 'dist/js', filter: 'isFile'},
+          // Move all pure (non-SASS) stylesheets into dist/css
+          {expand: true, cwd: 'src/css', src: ['*.css'], dest: 'dist/css', filter: 'isFile'},
+        ],
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['copy']);
 
 };
