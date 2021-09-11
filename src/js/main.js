93d98444014faa2df0e0c4144cb4a05c5a336f40
Karol Ciszek <kjc15@ic.ac.uk>
2021-09-11 15:45:54 +0100

    Create class representing an Unsplash photo
    These objects are built from the Unsplash API response and stored in the
    `photos` array.
    
    We make API calls from beeceptor.com to save on Unsplash calls. The
    Beeceptor endpoint always returns the same response from Unsplash

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,36 @@
   const API_ROOT = 'https://api.unsplash.com';
   const ACCESS_KEY = 'in_progress';
   const keyURI = 'http://localhost:3000/unsplash_key';
-  const photosURI = `${API_ROOT}/photos/?client_id=${ACCESS_KEY}`;
+  // const photosURI = `${API_ROOT}/photos/?client_id=${ACCESS_KEY}`;
+
+  // Make GET requests from a dummy API instead for development purposes
+  const photosURI = `https://responsive-image-grid.free.beeceptor.com`;
+
+  class UnsplashPhoto {
+    constructor (unsplData) {
+      this.unsplData = unsplData;
+      this.authorName = unsplData.user.name;
+      this.altText = unsplData.alt_description;
+
+      // Our tiles only show the small image
+      this.imgUrl = unsplData.urls.small;
+
+      this.href = unsplData.links.html;
+      this.id = unsplData.id;
+    }
+    // TODO: include likes of photo?
+  }
+
+  const photos = [];
+
+  function createPhotos (photoData) {
+    photoData.forEach(obj => {
+      photos.push(new UnsplashPhoto(obj));
+
+      // Dev purposes
+      console.log(photos);
+    });
+  }
 
   function fetchPhotos () {
     return fetch(photosURI)
@@ -12,7 +41,7 @@
 
   console.log("Hello world!");
 
-  // fetchPhotos()
-  //   .then(photos => console.log(photos))
-  //   .catch(err => console.log(err));
+  fetchPhotos()
+    .then(data => createPhotos(data))
+    .catch(err => console.log(err));
 })();
